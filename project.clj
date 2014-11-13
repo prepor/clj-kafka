02ed1a72e13a71bd777ccc5fffb864ee5f029473
Andrew Rudenko <ceo@prepor.ru>
2014-11-13 14:09:47 +0800

    logging stuff

--- project.clj
+++ project.clj
@@ -6,23 +6,27 @@
   :dependencies [
                  ;; conflicts
                  [org.clojure/data.json "0.2.3"]
-                 [org.slf4j/slf4j-api "1.7.2"]
-                 [log4j "1.2.17"]
 
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [clj-kafka "0.2.6-0.8"]
+                 [clj-kafka "0.2.6-0.8"
+                  :exclusions [[log4j]
+                               [org.slf4j/slf4j-simple]]]
                  [org.apache.commons/commons-pool2 "2.2"]
                  [org.apache.commons/commons-io "1.3.2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.stuartsierra/component "0.2.2"]
                  [com.taoensso/carmine "2.4.4"]
-                 [ru.prepor/utils "0.1.0"]
+                 [ru.prepor/utils "0.2.0"]
                  [org.apache.zookeeper/zookeeper "3.4.6"
                   :exclusions [[com.sun.jmx/jmxri]
                                [com.sun.jdmk/jmxtools]
                                [javax.jms/jms]
                                [javax.mail/mail]
                                [org.slf4j/slf4j-log4j12]
-                               [org.slf4j/slf4j-simple]]]
-                 [org.apache.curator/curator-test "2.6.0"]])
+                               [org.slf4j/slf4j-simple]
+                               [log4j]]]
+                 [org.apache.curator/curator-test "2.6.0"]]
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-api "1.7.7"]
+                                  [ch.qos.logback/logback-classic "1.1.2"]
+                                  [org.slf4j/log4j-over-slf4j "1.7.7"]]}})
