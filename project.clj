4a2f1b5f8f9278f322c4ad20171429b7ed6248a1
Andrew Rudenko <ceo@prepor.ru>
2015-01-08 16:55:40 +0800

    lein-release plugin: bumped version from 0.2.5 to 0.2.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ru.prepor/clj-kafka "0.2.5"
+(defproject ru.prepor/clj-kafka "0.2.6-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
