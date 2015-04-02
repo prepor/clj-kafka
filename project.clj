5139147ac28a7dbaaaddcd2febf120a73afc16e0
Andrew Rudenko <ceo@prepor.ru>
2015-04-02 17:09:03 +0300

    Version 0.3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ru.prepor/clj-kafka "0.3.6-SNAPSHOT"
+(defproject ru.prepor/clj-kafka "0.3.6"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
